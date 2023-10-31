b17fb9418a565a396eb33fc3c85c35dfe2adf4df
Potap0vsM <147475142+Potap0vsM@users.noreply.github.com>
2023-10-31 10:34:09 +0200

    Add files via upload

---
+++ watermarks yippee/watermarkMaker.py
@@ -0,0 +1,24 @@
+#Import required Image library
+from PIL import Image, ImageDraw, ImageFont
+
+#Create an Image Object from an Image
+im = Image.open('./main_photo.png')
+width, height = im.size
+
+draw = ImageDraw.Draw(im)
+text = "sample watermark"
+
+font = ImageFont.truetype('arial.ttf', 36)
+textwidth, textheight = draw.textsize(text, font)
+
+# calculate the x,y coordinates of the text
+margin = 10
+x = width - textwidth - margin
+y = height - textheight - margin
+
+# draw watermark in the bottom right corner
+draw.text((x, y), text, font=font)
+im.show()
+
+#Save watermarked image
+im.save('./photoMark.png')
